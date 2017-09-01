99b9958e84cc6d05767774b413b7ab1f52b08791
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-09-01 11:31:39 -0400

    Languages can have periods in the name (code)

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ const MATCH_RLINK = /^\[([^\]]+)\]\[([^\]]+)\]/;
 const MATCH_REFERENCE = /^(?:\s+|)\[([^\]]+)\]: ((?:http(?:s|):\/\/|)[a-zA-Z0-9%\-_?=.#]+)$/;
 const MATCH_PICTURE = /^!\[([^\]]+)\]\(([^\]]+)\)/;
 const MATCH_STRIKETHROUGH = /^~~([^\]]+?)~~/;
-const MATCH_CODE = /^(?:\s+|)```([a-zA-Z]+|)/;
+const MATCH_CODE = /^(?:\s+|)```([a-zA-Z._-]+|)/;
 const MATCH_INLINE_CODE = /^`([^`]+)`/;
 
 module.exports = {
